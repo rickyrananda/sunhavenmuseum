98c5d1cd6985df2e84e1b085d071325d34c89cfd
rickyrananda <rickyreza8@gmail.com>
2024-07-06 01:54:55 +0700

    update navbar (dropdown)
    Updated the navbar link with a dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,80 +6,63 @@ import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid'
 
 function Navbar() {
 
-    const [isActive, setIsActive] = useState(false)
+    const [dropDownStates, setDropDownStates] = useState([false, false, false]);
+
+    const handleDropdown = (index) => {
+        setDropDownStates((prevState) =>
+            prevState.map((isOpen, i) => (i === index ? !isOpen: false))
+        );
+
+        console.log(index);
+    };
+
+    const Bundles = [
+        {
+            name: 'The Hall of Gems',
+            options: ['Mana Bundle', 'Money Bundle', 'Golden Bunlde', 'Bars Bundle', 'Gems Bundle', 'Nel`Vari Mines Bundle', 'Withergate Mines Bundle'],
+        },
+        {
+            name: 'The Hall of Culture',
+            options: ['Spring Crops Bundle', 'Summer Crops Bundle', 'Fall Crops Bunlde', 'Winter Crops Bundle', 'Nel`Vari Crops Bundle', 'Withergate Crops Bundle', 'Flowers Bundle', 'Foraging Bundle', 'Exploration Bundle', 'Alchemy Bundle', 'Combat Bundle', 'Nel`Vari Temple Bundle'],
+        },
+        {
+            name: 'Aquarium',
+            options: ['Fishing Bundle', 'Srping Tank', 'Summer Tank', 'Fall Tank', 'Winter Tank', 'Nel`Vari Tank', 'Withergate Tank', 'Large Tank'],
+        },
+
+    ]
 
   return (
     <div className='flex h-[60px] bg-primary text-secondary '>
         <div className='w-full py-1 px-[200px] flex items-center justify-between'>
             <div className='flex items-center gap-2'>
                 <img className='h-12 w-12' src={images.logo} alt='Sun Haven Logo' />
-                <p>Museum Companion</p>
+                <p className='font-semibold'>Museum Companion</p>
             </div>
-            <div className='flex gap-5' id=''>
+            <div className='flex gap-5' id='navbarlink'>
                 {/* This is The Hall of Gems Section */}
-                <div className='flex items-center gap-2 cursor-pointer' >
-                    <p onClick={() => setIsActive(!isActive)}>The Hall of Gems</p>
-                    {isActive ? <ChevronDownIcon className='h-5 w-5' /> : <ChevronUpIcon className='h-5 w-5' /> }
-                </div>
-                <div>
-                    
-                </div>
+                    {Bundles.map((bundle, index) => (
+                        <div className='relative flex items-center gap-8 cursor-pointer' key={index} >
+                            <p className='flex items-center hover:scale-110' onClick={()=> handleDropdown(index)}>{bundle.name}</p>
 
-                {/* This is The Hall of Culture Section */}
-                <div className='flex items-center gap-2 cursor-pointer' onClick={() => setIsActive(!isActive)}>
-                    <p>The Hall of Culture</p>
-                    {isActive ? <ChevronDownIcon className='h-5 w-5' /> : <ChevronUpIcon className='h-5 w-5' /> }
-                </div>
-                <div>
+                            { dropDownStates[index] && (
+                                <div className='absolute top-8 text-primary bg-secondary min-w-[100px] rounded-sm py-2 z-20'>
+                                    {bundle.options.map((option, optIndex) => (
+                                        <a href="#"
+                                        key={optIndex}
+                                        className="block w-full px-4 py-2 text-xs text-primary hover:text-secondary hover:bg-primary ">
+                                            {option}
+                                        </a>
+                                        
+                                    ))}
+                                
+                                </div>
+                                
+                            )}
+                        </div>
+                    ))}
 
-                </div>
 
-                {/* This is Aquarium Section */}
-                <div className='flex items-center gap-2 cursor-pointer' onClick={() => setIsActive(!isActive)}>
-                    <p>Aquarium</p>
-                    {isActive ? <ChevronDownIcon className='h-5 w-5' /> : <ChevronUpIcon className='h-5 w-5' /> }
-                </div>
-                <div>
-
-                </div>
-                {/* <ul className='flex gap-8'>
-                    <li className=''><a>The Hall of Gems</a></li>
-                        <ul className="hidden">
-                            <li>Mana Bundle</li>
-                            <li>Money Bundle</li>
-                            <li>Golden Bundle</li>
-                            <li>Bars Bundle</li>
-                            <li>Gems Bundle</li>
-                            <li>Nel'Vari Mines Bundle</li>
-                            <li>Withergate Mines Bundle</li>
-                        </ul>
-                    <li><a>The Hall of Culture</a></li>
-                        <ul className="hidden">
-                            <li>Spring Crops Bundle</li>
-                            <li>Summer Crops Bundle</li>
-                            <li>Fall Crops Bundle</li>
-                            <li>Winter Crops Bundle</li>
-                            <li>Nel'Vari Crops Bundle</li>
-                            <li>Withergate Crops Bundle</li>
-                            <li>Flowers Bundle</li>
-                            <li>Foraging Bundle</li>
-                            <li>Exploration Bundle</li>
-                            <li>Alchemy Bundle</li>
-                            <li>Combat Bundle</li>
-                            <li>Nel'Vari Temple Bundle</li>
-                        </ul>
-                    <li><a>Aquarium</a></li>
-                        <ul className="hidden">
-                            <li>Fishing Bundle</li>
-                            <li>Spring Tank</li>
-                            <li>Summer Tank</li>
-                            <li>Fall Tank</li>
-                            <li>Winter Tank</li>
-                            <li>Nel'Vari Tank</li>
-                            <li>Withergate Tank</li>
-                            <li>Large Tank</li>
-                        </ul>
-                </ul>     */}
             </div>
         </div>
     </div>
