07357bf1fc9470ca4299adc9b90f09adbc7cab1f
rickyrananda <rickyreza8@gmail.com>
2024-07-05 16:20:04 +0700

    making small changes
    update the ui little bit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'primary': '#1A1B26',
+      'secondary': '#bf9553',
+      'default': '#4d4f59',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
